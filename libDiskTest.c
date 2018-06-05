5ccbb164df6f159673878901fe11c6dc4ef57cf2
Daniel Kim <phildankim@gmail.com>
2018-06-04 22:23:41 -0700

    last time i swear

--- libDiskTest.c
+++ libDiskTest.c
@@ -1,5 +1,15 @@
 #include "libDisk.h"
 
 int main() {
+   int d1 = openDisk("test.txt", BLOCKSIZE);
+   printf("%d\n", d1);
+   int d2 = openDisk("test1.txt", BLOCKSIZE);
+   printf("%d\n", d2);
+   char *buf = malloc(sizeof(int) * BLOCKSIZE);
+   int res = readBlock(d1, 1, buf);
+   printf("%s\n", buf);
+   char *buf2 = "poo";
+   writeBlock(d2, 1, buf);
    
+   return 0;
 }
